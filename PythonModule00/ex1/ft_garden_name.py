811aec71fd68411bd282cfd143314ed8adb878e5
Tolotra Rakotomalala Andriamahefa <toloandr@c1r4s1.42antananarivo.mg>
2026-05-28 13:52:13 +0300

    pythonModule00

---
+++ PythonModule00/ex1/ft_garden_name.py
@@ -0,0 +1,7 @@
+def ft_garden_name() -> None:
+    name: str = input("Enter garden name: ")
+    print(f"Garden: {name}")
+    print("Status: Growing well!")
+
+# if __name__ == "__main__":
+#     ft_garden_name()
