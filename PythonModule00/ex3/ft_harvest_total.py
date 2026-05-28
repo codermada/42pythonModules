b0351343751bdc145d5f5ee04c85fc54714bc25d
Tolotra Rakotomalala Andriamahefa <tolotra025@gmail.com>
2026-05-28 14:08:04 +0300

    pythonModule00/ex3

---
+++ PythonModule00/ex3/ft_harvest_total.py
@@ -0,0 +1,9 @@
+def ft_harvest_total() -> None:
+    d_1: int = int(input("Day 1 harvest: "))
+    d_2: int = int(input("Day 2 harvest: "))
+    d_3: int = int(input("Day 3 harvest: "))
+    print(f"Total harvest: {d_1 + d_2 + d_3}")
+
+
+# if __name__ == "__main__":
+#     ft_harvest_total()
