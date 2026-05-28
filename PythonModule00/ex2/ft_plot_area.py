6de72b6de62006d508aace0e303698f24d59d2af
Tolotra Rakotomalala Andriamahefa <tolotra025@gmail.com>
2026-05-28 14:02:55 +0300

    pythonModule00/ex2

---
+++ PythonModule00/ex2/ft_plot_area.py
@@ -0,0 +1,7 @@
+def ft_plot_area() -> None:
+    l: int = int(input("Enter length: "))
+    w: int = int(input("Enter width: "))
+    print(f"Plot area: {l * w}")
+
+# if __name__ == "__main__":
+#     ft_plot_area()
