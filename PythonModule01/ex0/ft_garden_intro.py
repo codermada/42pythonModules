3ace8b5fdd435595c68625ff8e32a372e8a32f12
Tolotra Rakotomalala Andriamahefa <tolotra025@gmail.com>
2026-06-03 09:26:20 +0300

    01/ex0

---
+++ PythonModule01/ex0/ft_garden_intro.py
@@ -0,0 +1,10 @@
+def ft_garden_intro(plant: str, height: float, age: int) -> None:
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {plant}")
+    print(f"Height: {height} cm")
+    print(f"Age: {age} days")
+
+
+if __name__ == "__main__":
+    ft_garden_intro("Rose", 25, 30)
+    print("\n=== End of Program ===")
